bb1598cdd4d312e1b19731f305abaf2ff5d337da
dileepyadavns <dileepyadavneo@gmail.com>
2022-07-15 17:57:29 +0530

    adding spell check for state name

--- actions/actions.py
+++ actions/actions.py
@@ -8,11 +8,10 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-#
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
-#
+
 #
 # class ActionHelloWorld(Action):
 #
@@ -27,11 +26,48 @@ import requests
 #
 #         return []
 
+# class ActionCoronaCases(Action):
+#     try:
+#         def name(self) -> Text:
+#          return "actions_corona_state_stat"
 
+#         def run(self, dispatcher: CollectingDispatcher,
+
+#              tracker: Tracker,
+#              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#          response=requests.get("https://data.covid19india.org/data.json").json()
+#          entities=tracker.latest_message['entities']
+#          print("Last message Now",entities)
+#          state=None
+#          for e in entities:
+#              if e["entity"]=="state":
+#                 state=e["value"]
+#          message="please enter valid state in india "
+#          for data in response["statewise"]:
+#              str1=state.title()
+#              str2=data['state']
+#              c=0
+#              j=0
+#              for i in str1:    
+#                  if str2.find(i)>= 0 and j == str1.find(i): 
+#                      c += 1
+#                  j += 1
+
+#              if c>int(len(str2)/2):
+#                  print(data)
+#                  message="Active: "+ data["active"]+"  Conformed: "+ data["confirmed"]+"  Recovered: "+ data["deltarecovered"] +" On: " + data["lastupdatedtime"]
+#              continue
+                 
+#          dispatcher.utter_message(message)
+#          return []
+#     except:
+#         pass
+
+  
 class ActionCoronaCases(Action):
     try:
         def name(self) -> Text:
-         return "action_corona_statewise_cases"
+         return "actions_corona_state_stat"
 
         def run(self, dispatcher: CollectingDispatcher,
 
@@ -45,14 +81,26 @@ class ActionCoronaCases(Action):
              if e["entity"]=="state":
                 state=e["value"]
          message="please enter valid state in india "
+         ct=0
+         max1=''
          for data in response["statewise"]:
-             if data["state"]==state.title():
-                 print(data)
-                 message="Active: "+ data["active"]+"  Conformed: "+ data["confirmed"]+"  Recovered: "+ data["deltarecovered"] +" On: " + data["lastupdatedtime"]
+             str1=state.title()
+             str2=data['state']
+             c=0
+             j=0
+             for i in str1:    
+                 if str2.find(i)>= 0 and j == str1.find(i): 
+                     c += 1
+                 j += 1
 
-
-
+             if c>ct:
+                 ct=c
+                 max1=data
+         message="Active: "+ max1["active"]+"  Conformed: "+ max1["confirmed"]+"  Recovered: "+ max1["deltarecovered"] +" On: " + max1["lastupdatedtime"]
+                   
          dispatcher.utter_message(message)
          return []
     except:
         pass
+
+
